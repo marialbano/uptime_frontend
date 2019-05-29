8a419f6f47c5cfa72eff497f1e7bb4354f2ffb7c
Mariana Albano <mariana.albano@mercadolivre.com>
2019-05-29 11:17:38 -0300

    organizated routes

---
+++ routes/office.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('./pages/office')
+})
+
+router.get('/new', function (req, res){
+    res.render('./pages/newOffice')
+});
+
+router.post('/new', function (req, res){
+    console.log(req.body)
+    res.render('./pages/office')
+});
+
+router.get('/edit/:id', function (req, res){
+
+    res.render('./pages/editOffice', {id: req.params})
+});
+
+router.post('/edit', function (req, res){
+    console.log(req.body)
+    res.render('./pages/office')
+});
+
+module.exports = router;
